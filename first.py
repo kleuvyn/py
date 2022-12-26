0794d992546641d2de05867fd1bac8a7a6bc6fc5
kleuvyn <kleuvyn@hotmail.com>
2022-12-25 23:41:27 -0300

    Merge remote-tracking branch 'origin/main'

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print('Olá mundo!')
+import time
+print('Olá seja bem vindo!')
+nome = input('Qual é o seu nome? \n ')
+print('Oi, {} poderia me informar a sua duvida ? \n ' .format(nome))
+print('...AGUARDE... \n ')
+time.sleep(2.0)
+print('ANAISANDO...')
+time.sleep(1.0)
+print('...')
+#editar com  tempo novamente
